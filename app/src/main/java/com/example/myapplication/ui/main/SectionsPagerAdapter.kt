95f032b302ecd7d00e6c355d8dcb6e5f1f2686cb
Nícolas Wojcichoski <nicolas.dsw@gmail.com>
2020-09-28 20:30:00 -0300

    feat: paid details fragment

--- app/src/main/java/com/example/myapplication/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/myapplication/ui/main/SectionsPagerAdapter.kt
@@ -6,16 +6,20 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.fragment.app.FragmentStatePagerAdapter
 import com.example.myapplication.R
+import com.superddaiupay.paid_details.PaidDetailsFragment
+import com.superddaiupay.paid_details.PaidDetailsParams
 import com.superddaiupay.payment_details.PaymentDetailsFragment
 import com.superddaiupay.payment_details.PaymentDetailsParams
 import com.superddaiupay.receipt.ReceiptFragment
 import com.superddaiupay.receipt.ReceiptParams
+import kotlinx.android.synthetic.main.activity_main_tabbed.view.*
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
     R.string.tab_text_2,
     R.string.tab_text_3,
-    R.string.tab_text_4
+    R.string.tab_text_4,
+R.string.tab_text_5
 )
 
 class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
@@ -27,6 +31,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
             1 -> SecondFragment()
             2 -> ReceiptFragment.newInstance(ReceiptParams())
             3 -> PaymentDetailsFragment.newInstance(PaymentDetailsParams())
+            4 -> PaidDetailsFragment.newInstance(PaidDetailsParams())
             else -> FirstFragment()
         }
     }
